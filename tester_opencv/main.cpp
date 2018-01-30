36d1ed590b94f215cb50fc863d4b19be12d0aa85
George Manolakis <georgemanolakis@dhcp-168-105-253-161.wireless.manoa.hawaii.edu>
2018-01-29 23:28:44 -1000

    Initial Commit

---
+++ tester_opencv/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tester_opencv
+//
+//  Created by George Manolakis on 1/29/18.
+//  Copyright © 2018 George Manolakis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
